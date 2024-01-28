a0c1694fba8819c5667667c64360d816e36b5320
ColeWilliams979 <157323328+ColeWilliams979@users.noreply.github.com>
2024-01-28 15:36:27 -0800

    Add files via upload

--- photoCollect.py
+++ photoCollect.py
@@ -3,23 +3,26 @@ import os
 import paramiko
 
 
-server = input("What is the IP of the pi: ")
-username = input("What is the username of the pi: ")
-password = input("What is the password: ")
+server = "10.1.10.147"
+username = "xacademy"
+password = "Gauss5050"
 
 photo_command = "libcamera-still -t 1 -o test{iteration}.jpg"
-download_command = "scp "+username+"@"+server+":/home/xacademy/test.jpg /Users/X\" \"Academy/photos"
+download_command = "scp {username}@{server}:/home/xacademy/test{iteration}.jpg /Users/X\" \"Academy/photos"
 
 
 ssh = paramiko.SSHClient()
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 ssh.connect(server, username=username, password=password)
 
 
 for i in range(10):
     
     ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(photo_command.format(iteration=i))
-    os.system(download_command)
-    
+    os.system(download_command.format(username=username, server=server, iteration=i))
+
+
+
 
 
 
